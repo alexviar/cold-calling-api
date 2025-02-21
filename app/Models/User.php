6456b1c3d43b980ba678f30471bad8e29f7490d9
alexviar <alexanderviar@gmail.com>
2025-02-21 13:43:05 -0400

    feat(auth): signin

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -32,6 +36,13 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => Hash::make($value)
+        );
+    }
 
     /**
      * Get the attributes that should be cast.
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\TelnyxController;
 use App\Models\Campaign;
@@ -9,6 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->prefix('auth')->group(function () {
+    Route::post('login', 'login');
+});
 
 Route::controller(CampaignController::class)->prefix('campaigns')->group(function () {
     Route::post('/', [CampaignController::class, 'store']);
